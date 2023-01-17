27ed56cf58008d2baa66d8350bc36046d7fa2ac7
_brln.by <2019110015@students.uigm.ac.id>
2023-01-17 21:49:14 +0700

    create back to top button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import type { HeadFC, PageProps } from 'gatsby'
 import React from 'react'
 import { HiOutlineChevronDoubleDown } from 'react-icons/hi'
-import { Cirle } from '../components/Miscellaneous'
 import BaseLayout from '../layouts/Base.layout'
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <BaseLayout>
-      <Cirle className='right-14 top-28' />
-      <Cirle className='left-14 top-28' />
-      <Cirle className='left-96 top-56' />
-      <Cirle className='right-[32rem] bottom-56' />
-      <Cirle className='left-[30rem] top-[50rem]' />
       <div className='container flex flex-col items-center justify-start w-full min-h-screen px-2 pt-48 mx-auto space-y-4'>
         <h1 className='z-0 text-4xl font-bold text-center text-catalina-blue-900'>
           Selamat Datang di Website Saya
@@ -31,8 +25,10 @@ const IndexPage: React.FC<PageProps> = () => {
           <HiOutlineChevronDoubleDown className='w-8 h-8' />
         </button>
       </div>
-      <div className='container w-full min-h-screen px-2 mx-auto bg-gradient-to-r'></div>
-      <div className='w-full min-h-screen bg-gradient-to-r'></div>
+      <div className='container w-full min-h-screen px-2 mx-auto'>
+        
+      </div>
+      <div className='w-full min-h-screen'></div>
       <div className='flex items-center justify-center w-full min-h-screen'></div>
     </BaseLayout>
   )
