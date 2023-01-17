e19473cbcb8b59aecc76ad2dad897b61e5b8d162
_brln.by <90144633+bluntswordman@users.noreply.github.com>
2023-01-17 20:47:28 +0700

    add prettier (#23)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,25 @@
-import { HeadFC, Link, PageProps } from "gatsby"
-import React from "react"
+import { HeadFC, Link, PageProps } from 'gatsby'
+import React from 'react'
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main className="container flex flex-col items-center justify-center w-full min-h-screen mx-auto space-y-6">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold text-catalina-blue-900">404</h1>
-        <p className="text-xl font-medium capitalize text-catalina-blue-700">page not found</p>
+    <main className='container flex flex-col items-center justify-center w-full min-h-screen mx-auto space-y-6'>
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-6xl font-bold text-catalina-blue-900'>404</h1>
+        <p className='text-xl font-medium capitalize text-catalina-blue-700'>
+          page not found
+        </p>
       </div>
-      <Link to="/" className="px-5 py-2 font-semibold text-white transition-all duration-300 rounded-lg bg-catalina-blue-900 hover:bg-catalina-blue-700">Go home</Link>
+      <Link
+        to='/'
+        className='px-5 py-2 font-semibold text-white transition-all duration-300 rounded-lg bg-catalina-blue-900 hover:bg-catalina-blue-700'
+      >
+        Go home
+      </Link>
     </main>
-  );
-};
+  )
+}
 
-export default NotFoundPage;
+export default NotFoundPage
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => <title>Not found</title>
