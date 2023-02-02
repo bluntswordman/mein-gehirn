8fd7b02680d2dd1e3f62f5f22d77860868dc2d48
_brln.by <2019110015@students.uigm.ac.id>
2023-02-02 12:55:48 +0700

    custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,25 +1,19 @@
-import { HeadFC, Link, PageProps } from 'gatsby';
-import React from 'react';
+import { HeadFC, Link, PageProps } from 'gatsby'
+import React from 'react'
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main className="container flex flex-col items-center justify-center w-full min-h-screen mx-auto space-y-6 bg-slate-900">
+    <main className="flex flex-col items-center justify-center w-full min-h-screen mx-auto space-y-6 bg-slate-900">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold text-catalina-blue-900">404</h1>
-        <p className="text-xl font-medium capitalize text-amber-500">
-          page not found
-        </p>
+        <h1 className="text-3xl md:text-6xl lg:text-9xl font-extrabold text-amber-500 select-none animate-pulse">404</h1>
+        <p className="text-2xl font-medium capitalize text-amber-500 select-none">page not found</p>
       </div>
-      <Link
-        to="/"
-        className="px-5 py-2 font-semibold text-neutral-300 transition-all duration-300 rounded-lg bg-catalina-blue-900 hover:bg-amber-500"
-      >
-        Go home
+      <Link to="/" className="px-4 py-1.5 text-sm font-extrabold capitalize text-slate-900 border border-slate-700 transition-all duration-300 rounded-md bg-amber-500 hover:bg-amber-600">Go home
       </Link>
     </main>
-  );
-};
+  )
+}
 
-export default NotFoundPage;
+export default NotFoundPage
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => <title>Not found</title>
